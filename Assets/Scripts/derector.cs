0033838118e30ae83703a616be4eee58454aa933
MA1H1ME <71712490+Sema168@users.noreply.github.com>
2021-10-27 15:09:45 +0900

    2021/1027_作業準備_監督スクリプトを作成

---
+++ Assets/Scripts/derector.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class derector : MonoBehaviour
+{
+
+   //Fキーを押してスキルのスライダーを右に回転します
+    //void Start()
+    //{
+            
+    // }
+   
+    //void Update()
+    //{
+        
+    //}
+
+    void main()
+    {
+        if (Input.GetKeyDown("F"))
+        {
+            SkillSelect();
+        }
+    }
+
+    public void SkillSelect()
+    {
+
+    }
+}
