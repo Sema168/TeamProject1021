095c3968044fb428b4bf660febcec5fd8ce7084c
1 <1>
2021-11-04 14:36:03 +0900

    反射のスクリプトと移動スクリプトの調整

---
+++ Assets/Bounce.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+    private Vector2 lastVelocity;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        this.rb = this.GetComponent<Rigidbody2D>();
+
+        //èüéËÇ…ìÆÇ©Ç∑ÇΩÇﬂÇÃÉvÉçÉOÉâÉÄÅAå„ÇŸÇ«è¡ÇµÇ‹Ç∑
+        rb.velocity = new Vector2(0, 4);
+    }
+
+    void FixedUpdate()
+    {
+        this.lastVelocity = this.rb.velocity;
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        Vector2 refrectVec = Vector2.Reflect(this.lastVelocity, coll.contacts[0].normal);
+        this.rb.velocity = refrectVec;
+    }
+}
