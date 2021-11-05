0d73ba750400403aae08c5f5685d2c518c2bc1b0
koronda <sffola1223@gmail.com>
2021-11-05 10:48:42 +0900

    Playerに近いたら止まって弾を発射

--- Assets/Enemy/hani.cs
+++ Assets/Enemy/hani.cs
@@ -6,22 +6,31 @@ public class hani : MonoBehaviour
 {
     public GameObject player;
     public GameObject tama;
+    public GameObject enemy;
     //一秒ごとに弾を発射するためのもの
+    bool ha = true;
     private float targetTime = 1.0f;
     private float currentTime = 0;
+    public int speed = 5;  //オブジェクトが自動で動くスピード調整
+    Vector3 movePosition;//②オブジェクトの目的地を保存
     void Start()
     {
+        movePosition = moveRandomPosition();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (ha)
+        {
+            idou();
+        }
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.tag == "player")
         {
+            ha = false;
             //一秒経つごとに弾を発射する
             currentTime += Time.deltaTime;
             if (targetTime < currentTime)
@@ -49,4 +58,24 @@ public class hani : MonoBehaviour
         }
 
     }
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        ha = true;
+    }
+    void idou()
+    {
+
+        if (movePosition == enemy.transform.position)  //②playerオブジェクトが目的地に到達すると、
+        {
+            movePosition = moveRandomPosition();  //②目的地を再設定
+        }
+        this.enemy.transform.position = Vector3.MoveTowards(enemy.transform.position, movePosition, speed * Time.deltaTime);  //①②playerオブジェクトが, 目的地に移動, 移動速度
+
+    }
+    private Vector3 moveRandomPosition()  // 目的地を生成、xとyのポジションをランダムに値を取得 
+    {
+        Vector3 randomPosi = new Vector3(Random.Range(-7, 7), Random.Range(-4, 4), 5);
+        return randomPosi;
+    }
+
 }
