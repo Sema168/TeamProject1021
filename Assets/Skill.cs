e2462e4f71d049bffe330c463d24d65d4fd864c8
Sema168 <y.debu0725@gmail.com>
2021-11-12 12:19:18 +0900

    スキル用の監督オブジェクトとスクリプト作りました

---
+++ Assets/Skill.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            Skill1();
+        }
+        if (Input.GetKeyDown("2"))
+        {
+            Skill2();
+        }
+        if (Input.GetKeyDown("3"))
+        {
+            Skill3();
+        }
+    }
+    void Skill1()
+    {
+
+    }
+    void Skill2()
+    {
+
+    }
+    void Skill3()
+    {
+
+    }
+}
