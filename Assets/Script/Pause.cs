a44157269c9bdfaea206ecef20e3ede1829c23f2
1 <90373881+r0cky2926@users.noreply.github.com>
2022-01-13 16:31:17 +0900

    GameSceneをある程度復元しました

---
+++ Assets/Script/Pause.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject pauseUI;
+	[SerializeField]
+	private GameObject panel;
+	private bool isPush = false;
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape) || isPush == true)
+		{
+			pauseUI.SetActive(!pauseUI.activeSelf);
+			panel.SetActive(!panel.activeSelf);
+			isPush = false;
+
+			if (pauseUI.activeSelf)
+			{
+				Time.timeScale = 0f;
+			}
+			else
+			{
+				Time.timeScale = 1f;
+			}
+		}
+	}
+
+	public void PushButton()
+	{
+		isPush = true;
+	}
+	////if (Mathf.Approximately(Time.timeScale, 0f))
+ //       //{
+ //           return;
+ //       }
+}
